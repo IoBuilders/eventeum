3f9a2439350c5a470d3f8b592b9c03723a6956ca
pedro.polo <pedro.polo@ecomi.com>
2025-02-17 16:44:55 +0100

    adjust number serializer

--- core/src/main/java/net/consensys/eventeum/dto/event/serializer/NumberParameterSerializer.java
+++ core/src/main/java/net/consensys/eventeum/dto/event/serializer/NumberParameterSerializer.java
@@ -1,35 +1,40 @@
 package net.consensys.eventeum.dto.event.serializer;
 
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.core.type.WritableTypeId;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
+import java.io.IOException;
 import net.consensys.eventeum.dto.event.parameter.NumberParameter;
 import org.springframework.boot.jackson.JsonComponent;
 
-import java.io.IOException;
-
 @JsonComponent
-public class NumberParameterSerializer
-        extends JsonSerializer<NumberParameter> {
-    @Override
-    public void serialize(NumberParameter value,
-                          JsonGenerator jsonGenerator,
-                          SerializerProvider serializerProvider)
-            throws IOException {
-      jsonGenerator.writeStartObject();
-      jsonGenerator.writeStringField("type", value.getType());
-      jsonGenerator.writeStringField("value", value.getValueString());
-      jsonGenerator.writeEndObject();
-    }
+public class NumberParameterSerializer extends JsonSerializer<NumberParameter> {
+  @Override
+  public void serialize(
+      NumberParameter value, JsonGenerator gen, SerializerProvider serializerProvider)
+      throws IOException {
+    gen.writeStartObject();
+    writeFields(value, gen);
+    gen.writeEndObject();
+  }
 
-    @Override
-    public void serializeWithType(NumberParameter value, JsonGenerator gen,
-                                  SerializerProvider provider, TypeSerializer typeSer)
-            throws IOException {
+  @Override
+  public void serializeWithType(
+      NumberParameter value, JsonGenerator gen, SerializerProvider provider, TypeSerializer typeSer)
+      throws IOException {
 
-        typeSer.writeTypePrefixForObject(value, gen);
-        serialize(value, gen, provider);
-        typeSer.writeTypeSuffixForObject(value, gen);
-    }
+    WritableTypeId typeId =
+        typeSer.writeTypePrefix(gen, typeSer.typeId(value, JsonToken.START_OBJECT));
+
+    writeFields(value, gen);
+    typeSer.writeTypeSuffix(gen, typeId);
+  }
+
+  private void writeFields(NumberParameter value, JsonGenerator gen) throws IOException {
+    gen.writeStringField("type", value.getType());
+    gen.writeStringField("value", value.getValueString());
+  }
 }
