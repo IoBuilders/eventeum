fa7fac432f7d867e2d8c677c78963cb3c8c1eb66
Sergio Mendoza <sergio@io.builders>
2025-03-13 12:23:05 +0100

    Revert "Merge pull request #25 from IoBuilders/ARQ-228-eventeum-fix-transactions-amount-serialization-in-kafka"
    This reverts commit cee2621b0648f6c7f837d4907d6703ab547a151b, reversing
    changes made to e303358cc762a0c6a736fc0dc05a9c165a6add52.

--- core/src/main/java/net/consensys/eventeum/utils/JSON.java
+++ core/src/main/java/net/consensys/eventeum/utils/JSON.java
@@ -16,9 +16,6 @@ package net.consensys.eventeum.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import net.consensys.eventeum.dto.event.parameter.NumberParameter;
-import net.consensys.eventeum.dto.event.serializer.NumberParameterSerializer;
 
 /**
  * Useful JSON based utility methods.
@@ -30,10 +27,6 @@ public class JSON {
 
   public static String stringify(Object object) {
     try {
-      SimpleModule module = new SimpleModule();
-      module.addSerializer(NumberParameter.class, new NumberParameterSerializer());
-      objectMapper.registerModule(module);
-
       return objectMapper.writeValueAsString(object);
     } catch (JsonProcessingException e) {
       e.printStackTrace();
